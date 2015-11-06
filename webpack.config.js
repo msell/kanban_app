70649dabd5dd94d216c44e0e9902c5ab0bc17172
Matt Sell <mwsell@gmail.com>
2015-11-05 21:54:17 -0600

    configure npm start to launch browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path');
+var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
@@ -11,9 +13,22 @@ module.exports = {
     path: BUILD_PATH,
     filename: 'bundle.js'
   },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    // parse host and port from env for ease of cusomization
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       title: 'Kanban app'
+    }),
+    new OpenBrowserPlugin({
+      url: 'http://localhost:8080'
     })
   ]
 };
