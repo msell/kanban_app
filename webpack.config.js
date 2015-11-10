94f6780b8a48b549cf5c581e715b5907cba202be
Matt Sell <mwsell@gmail.com>
2015-11-09 21:37:15 -0600

    create jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,30 @@
 var path = require('path');
-var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
-var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var webpack = require('webpack');
 var merge = require('webpack-merge');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
-var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+process.env.BABEL_ENV = TARGET;
 
 var common = {
   entry: APP_PATH,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     loaders: [
       {
         test: /\.css$/,
         loaders: ['style', 'css'],
-        inlculde: APP_PATH
+        include: APP_PATH
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: APP_PATH
       }
     ]
   },
@@ -27,56 +35,22 @@ var common = {
   ]
 };
 
-if(TARGET === 'start' || !TARGET){
+if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devtool: 'eval-source-map',
-    devServer:{
+    devServer: {
       historyApiFallback: true,
       hot: true,
       inline: true,
-      progress: true
+      progress: true,
+
+      // parse host and port from env so this is easy
+      // to customize
+      host: process.env.HOST,
+      port: process.env.PORT
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new OpenBrowserPlugin({
-            url: 'http://localhost:8080'
-          })
+      new webpack.HotModuleReplacementPlugin()
     ]
   });
 }
-
-
-// module.exports = {
-//   entry: APP_PATH,
-//   output: {
-//     path: BUILD_PATH,
-//     filename: 'bundle.js'
-//   },
-//   devServer: {
-//     historyApiFallback: true,
-//     hot: true,
-//     inline: true,
-//     progress: true,
-//     // parse host and port from env for ease of cusomization
-//     host: process.env.HOST,
-//     port: process.env.PORT
-//   },
-//   plugins: [
-//     new webpack.HotModuleReplacementPlugin(),
-//     new HtmlwebpackPlugin({
-//       title: 'Kanban app'
-//     }),
-//     new OpenBrowserPlugin({
-//       url: 'http://localhost:8080'
-//     })
-//   ],
-//   module:{
-//     loaders:[
-//       {
-//         test: /\.css$/,
-//         loaders: ['style', 'css'],
-//         include: APP_PATH
-//       }
-//     ]
-//   }
-// };
