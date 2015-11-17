ff16dae7b5350705773e1838d40fde96d00ede30
Matt Sell <mwsell@gmail.com>
2015-11-16 22:16:48 -0600

    refactor Notes into component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,7 +1,35 @@
 import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+import Notes from './Notes.jsx';
 
-export default () =>{
-  return <Note />
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { 
+			notes: [
+			{
+				id: uuid.v4(),
+				task: 'Learn Webpack'
+			}, 
+			{
+				id: uuid.v4(),
+				task: 'Learn React'
+			},
+			{
+				id: uuid.v4(),
+				task: 'Do laundry'
+			} 
+			]
+		}; 
+	}
+
+	render() {
+		const notes = this.state.notes;
+		return (
+			<div>
+			<Notes items={notes} />
+			</div>
+			);
+	}
 }
